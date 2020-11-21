27f62fea41ae5042d8e583aae780a5f813c9cf80
Kang Yi <kangyte@gmail.com>
2020-11-21 04:06:19 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+(function(){
+  //add this line for each website you want to open
+  window.open("http://www.facebook.com/",'');
+  window.open("http://www.kclau.com/",'');
+  window.open("coolerones.com/",'');
+  window.open("companyforyouthsome.com/",'');
+  window.open("futurgift.com/",'');
+  window.open("http://www.innotvgood.com/",'');
+
+})()
